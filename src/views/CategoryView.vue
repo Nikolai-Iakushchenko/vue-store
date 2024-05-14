12d36dd5477ab4c1ae3748ff3be7669bfb2975c4
Nikolai <nick22783@gmail.com>
2024-05-14 22:38:30 +0300

    add CategoryView.vue

---
+++ src/views/CategoryView.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+type PropTypes = {
+  categoryName: string
+}
+
+const props = defineProps<PropTypes>()
+</script>
+
+<template>
+  <h2>Category: {{ categoryName }}</h2>
+  <p>id:</p>
+</template>
+
+<style scoped></style>
