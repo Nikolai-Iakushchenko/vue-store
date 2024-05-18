764d734ee493f3aba124648bf9fdfaca6f62c922
Nikolai <nick22783@gmail.com>
2024-05-18 12:31:46 +0300

    use store in ProductView.vue

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,15 +1,8 @@
 <script lang="ts" setup>
 import { useCategoryStore } from '@/stores/CategoryStore'
-import type { Category } from '@/types/categoriesTypes'
 import { computed, ref, watch } from 'vue'
-import CategoryItem from '@/components/CategoryItem.vue'
 import ProductItem from '@/components/ProductItem.vue'
 import { useRoute } from 'vue-router'
-import { getProducts } from '@/utils/getProducts'
-import type { Product } from '@/types/productTypes'
-import type { Ref } from 'vue'
-import type { Products } from '@/types/commonTypes'
-import { getCategory } from '@/utils/getCategory'
 
 const categoryStore = useCategoryStore()
 
@@ -18,8 +11,6 @@ type PropTypes = {
 }
 
 const props = defineProps<PropTypes>()
-const { categoryId } = props
-// console.log('props', props)
 
 categoryStore.fetchCategory(props.categoryId)
 const category = computed(() => {
@@ -30,25 +21,6 @@ const products = computed(() => {
 })
 
 const route = useRoute()
-
-// const category = ref<Category | null>(null)
-// const products = ref<Ref<Product[]> | null>(null)
-// const isLoading = ref(false)
-// const error = ref(null)
-
-// const fetchProducts = async (categoryId) => {
-//   error.value = category.value = products.value = null
-//   isLoading.value = true
-//
-//   try {
-//     category.value = await getCategory(categoryId)
-//     products.value = await getProducts(category.value.productIds)
-//   } catch (error) {
-//     error.value = error.toString()
-//   } finally {
-//     isLoading.value = false
-//   }
-// }
 
 watch(() => route.params.categoryId, categoryStore.fetchCategory, { immediate: true })
 </script>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,8 +1,18 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
+import { useCategoryStore } from '@/stores/CategoryStore'
+import { computed } from 'vue'
 
 const route = useRoute()
 const productId = route.params.productId
+
+const categoryStore = useCategoryStore()
+
+const product = computed(() => {
+  return categoryStore.products.find((product) => product.id === productId)
+})
+
+console.log('product', product)
 </script>
 
 <template>
