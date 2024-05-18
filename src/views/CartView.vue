20953f9ae4817024e0c7845aff40f855d698b118
Nikolai <nick22783@gmail.com>
2024-05-18 16:20:12 +0300

    show items in CartView.vue

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,6 +1,20 @@
+<script setup>
+import { useCartStore } from '@/stores/CartStore'
+import { computed } from 'vue'
+
+const cartStore = useCartStore()
+const cart = computed(() => {
+  return cartStore.cart
+})
+</script>
+
 <template>
-    <h2>Cart</h2>
+  <h2>Cart</h2>
+  <ul>
+    <li v-for="item in cart" :key="item.id" :item="item">
+      {{ item.name }}
+    </li>
+  </ul>
 </template>
 
-<style module>
-</style>
+<style module></style>
