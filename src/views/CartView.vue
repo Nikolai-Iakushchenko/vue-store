03ca9951ab63e3691803a9693bdcaf4094fc67d3
Nikolai <nick22783@gmail.com>
2024-05-19 13:45:54 +0300

    add Congratulations on your purchase

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,12 +1,18 @@
 <script setup>
 import { useCartStore } from '@/stores/CartStore'
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import CartItem from '@/components/CartItem.vue'
 
 const cartStore = useCartStore()
 const cart = computed(() => {
   return cartStore.cart
 })
+
+const purchaseMade = ref(false)
+
+const placePurchase = () => {
+  purchaseMade.value = true
+}
 </script>
 
 <template>
@@ -14,7 +20,10 @@ const cart = computed(() => {
   <ul :class="$style.cartList">
     <CartItem v-for="item in cart" :key="item.cartId" :cartItem="item" />
   </ul>
-  <button v-if="cartStore.numberOfItems">Place Order</button>
+  <button v-if="cartStore.numberOfItems" @click="placePurchase">
+    Place Order
+  </button>
+  <p v-if="purchaseMade">Congratulations on your purchase!</p>
 </template>
 
 <style module>
