4293fddcbcd6511e32862fc0531855b57c88dfa8
Nikolai <nick22783@gmail.com>
2024-05-19 15:06:21 +0300

    clean up types

--- src/types/commonTypes.ts
+++ src/types/commonTypes.ts
@@ -1,15 +1,5 @@
-import type { Category } from '@/types/categoriesTypes'
-import type { Product } from '@/types/productTypes'
-
 export type ImageDetails = {
   url: string
   width: number
   height: number
-}
-
-export type Translations = { '<ISO_LANG_CODE>': string }
-
-export type Alts = {
-  main: string
-  '<ISO_LANG_CODE>': string
 }
